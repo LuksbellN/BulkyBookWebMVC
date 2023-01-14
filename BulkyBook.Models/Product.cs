ed7e67bea602fcd338df61e76b64746cd9954c77
Luksbell <belchior1100@gmail.com>
2023-01-14 13:41:10 -0300

    Creating Upsert View, ProductViewModel, and Custom validation to image field || Get upsert done

--- BulkyBook.Models/Product.cs
+++ BulkyBook.Models/Product.cs
@@ -22,6 +22,9 @@ namespace BulkyBook.Models
         public string Author { get; set; }
         [Required]
         [Range(1, 10000)]
+        public double ListPrice { get; set; }
+        [Required]
+        [Range(1, 10000)]
         public double Price { get; set; }
         [Required]
         [Range(1, 10000)]
@@ -29,12 +32,15 @@ namespace BulkyBook.Models
         [Required]
         [Range(1, 10000)]
         public double Price100 { get; set; }
+        [Display(Name ="Image")]
         public string ImageUrl { get; set; }
         [Required]
+        [Display(Name ="Category")]
         public int CategoryId { get; set; }
         [ForeignKey("CategoryId")]
         public Category Category { get; set; }
         [Required]
+        [Display(Name = "Cover Type")]
         public int CoverTypeId { get; set; }
         public CoverType CoverType { get; set; }
 
---
+++ BulkyBookWeb/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,107 @@
+﻿using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+using BulkyBook.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace BulkyBookWeb.Areas.Admin.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ProductController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+
+        public IActionResult Index()
+        {
+            IEnumerable<CoverType> coverTypes = _unitOfWork.CoverType.GetAll();
+            return View(coverTypes);
+        }
+
+        //GET
+        public IActionResult Upsert(int? id)
+        {
+            ProductVM productVM = new()
+            {
+                Product = new(),
+                CategoryList = _unitOfWork.Category.GetAll().Select(i => new SelectListItem
+                {
+                    Text = i.Name,
+                    Value = i.Id.ToString()
+                }),
+                CoverTypeList = _unitOfWork.CoverType.GetAll().Select(i => new SelectListItem
+                {
+                    Text = i.Name,
+                    Value = i.Id.ToString()
+                })
+
+            };
+            if (id == null || id == 0)
+            {
+                //Create Product
+                //ViewBag.CategoryList = CategoryList;
+                //ViewData["CoverTypeList"] = CoverTypeList;
+                return View(productVM);
+            }
+            else
+            {
+                //Update Product
+            }
+
+            return View(productVM);
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert(ProductVM obj, IFormFile file)
+        {
+            if (ModelState.IsValid)
+            {
+                //_unitOfWork.CoverType.Update(obj);
+                _unitOfWork.Save();
+                TempData["success"] = "Cover Type updated successfully";
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        //GET
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var obj = _unitOfWork.CoverType.GetFirstOrDefault(u => u.Id == id);
+
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePOST(int? id)
+        {
+            var obj = _unitOfWork.CoverType.GetFirstOrDefault(u => u.Id == id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _unitOfWork.CoverType.Remove(obj);
+            _unitOfWork.Save();
+            TempData["success"] = "Cover Type deleted successfully";
+            return RedirectToAction("Index");
+        }
+    }
+}
